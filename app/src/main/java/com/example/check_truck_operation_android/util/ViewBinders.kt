cd17a82db339301c2b93261e32c0b427f2be512d
junjange <wnswkd486@naver.com>
2023-01-16 00:57:11 +0900

    [CheckTruckOperation] clean architecture setting > presentation base setting

---
+++ app/src/main/java/com/example/check_truck_operation_android/util/ViewBinders.kt
@@ -0,0 +1,11 @@
+package com.example.check_truck_operation_android.util
+
+import android.view.View
+import androidx.databinding.BindingAdapter
+
+@BindingAdapter("onSingleClick")
+fun View.setOnSingleClickListener(clickListener: View.OnClickListener?) {
+    clickListener?.also {
+        setOnClickListener(OnSingleClickListener(it))
+    } ?: setOnClickListener(null)
+}
